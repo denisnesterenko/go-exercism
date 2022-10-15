fc9111a6b48e3ba0cdf2ee11b672b90e36311659
Denis Nesterenko <d.nesterenko@aol.com>
2022-10-15 13:38:26 +0300

    Add atbash chiper implementation

--- exercises/practice/atbash-cipher/atbash_cipher.go
+++ exercises/practice/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,25 @@
 package atbash
 
+import "unicode"
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	var encodedString string
+	counter := 0
+	for _, char := range s {
+		if char == ' ' || unicode.IsPunct(char) {
+			continue
+		}
+		if counter == 5 {
+			encodedString += " "
+			counter = 0
+		}
+		if unicode.IsLetter(char) {
+			lowered := unicode.ToLower(char)
+			encodedString += string('z' + ('a' - lowered))
+		} else {
+			encodedString += string(char)
+		}
+		counter++
+	}
+	return encodedString
 }

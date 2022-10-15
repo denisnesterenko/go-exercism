ea3ed1c71339f286d19dc50ef03a8068b4ed5126
Denis Nesterenko <d.nesterenko@aol.com>
2022-10-15 12:53:46 +0300

    Add acronym function

--- exercises/practice/acronym/acronym.go
+++ exercises/practice/acronym/acronym.go
@@ -5,11 +5,23 @@
 // https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-// Abbreviate should have a comment documenting it.
+import (
+	"unicode"
+)
+
+// Transforms trimmed sentence to the abbreviation
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var result string
+	wordEnded := true
+	for _, char := range s {
+		if unicode.IsLetter(char) && wordEnded {
+			result += string(unicode.ToUpper(char))
+			wordEnded = false
+		} else if char == ' ' || char == '-' {
+			wordEnded = true
+		} else {
+			continue
+		}
+	}
+	return result
 }

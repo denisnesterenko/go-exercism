1146fd75cc88f7956ecb76f58302862787a9b2e4
Denis Nesterenko <d.nesterenko@aol.com>
2022-10-16 21:06:43 +0300

    Implement ISBN verifier

--- exercises/practice/isbn-verifier/isbn_verifier.go
+++ exercises/practice/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,33 @@
 package isbn
 
+import (
+	"strconv"
+	"strings"
+)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	counter, isbnSum := 0, 0
+	isbnCleared := strings.Replace(isbn, "-", "", 13)
+	if len(isbnCleared) != 10 {
+		return false
+	}
+
+	for i := 0; i < 10; i++ {
+		num, err := strconv.Atoi(string(isbnCleared[i]))
+		// Handle the case when the last element could be X
+		// which represents the number 10
+		if err != nil {
+			if i != 9 {
+				continue
+			}
+			if isbnCleared[i] == 'X' {
+				num = 10
+			} else {
+				return false
+			}
+		}
+		counter++
+		isbnSum += num * (11 - counter)
+	}
+	return isbnSum%11 == 0
 }

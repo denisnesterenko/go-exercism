deb996577fdfefb4c75f33ae3b8b6ee0d242117c
Denis Nesterenko <d.nesterenko@aol.com>
2022-10-19 22:26:34 +0300

    Implement Diffie-Hellman mechanism

--- exercises/practice/diffie-hellman/diffie_hellman.go
+++ exercises/practice/diffie-hellman/diffie_hellman.go
@@ -1,22 +1,31 @@
 package diffiehellman
 
-import "math/big"
+import (
+	"math/big"
+	"math/rand"
+	"time"
+)
 
 // Diffie-Hellman-Merkle key exchange
 // Private keys should be generated randomly.
 
+var seed = rand.New(rand.NewSource(time.Now().Unix()))
+
 func PrivateKey(p *big.Int) *big.Int {
-	panic("Please implement the PrivateKey function")
+	two := big.NewInt(2)
+	privateKey := new(big.Int).Sub(p, two)
+	return privateKey.Add(two, privateKey.Rand(seed, privateKey))
 }
 
 func PublicKey(private, p *big.Int, g int64) *big.Int {
-	panic("Please implement the PublicKey function")
+	return new(big.Int).Exp(big.NewInt(g), private, p)
 }
 
-func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
-	panic("Please implement the NewPair function")
+func NewPair(p *big.Int, g int64) (privateKey, public *big.Int) {
+	privateKey = PrivateKey(p)
+	return privateKey, PublicKey(privateKey, p, g)
 }
 
 func SecretKey(private1, public2, p *big.Int) *big.Int {
-	panic("Please implement the SecretKey function")
+	return new(big.Int).Exp(public2, private1, p)
 }

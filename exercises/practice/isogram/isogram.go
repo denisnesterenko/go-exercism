975c147c7215f627e53560654f4656b43d7011f9
Denis Nesterenko <d.nesterenko@aol.com>
2022-10-22 12:00:36 +0300

    Implement isogram detecting function

--- exercises/practice/isogram/isogram.go
+++ exercises/practice/isogram/isogram.go
@@ -1,5 +1,17 @@
 package isogram
 
+import "strings"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	visited := make(map[rune]bool, 0)
+	for _, letter := range strings.ToLower(word) {
+		if letter == '-' || letter == ' ' {
+			continue
+		}
+		if visited[letter] {
+			return false
+		}
+		visited[letter] = true
+	}
+	return true
 }
